fa0a70a9599f9000722f392334b880a3a3d02369
adarshrawat <arawat@grepruby.io>
2024-01-19 16:49:29 +0530

    fixes

--- swagger.mjs
+++ swagger.mjs
@@ -10,6 +10,12 @@ const swaggerConfig = {
     description: 'API documentation [StoreFront]',
     version: '1.0.0', // Specify your API version
   },
+  tags: [
+    {
+      name: 'StoreFront APIs',
+      description: 'Custom default tag description',
+    }
+    ,],
   host:  'localhost:5000', // Replace 'your-server-ip' with the actual IP address or hostname
   basePath: '/store',
   schemes: ['http'],
